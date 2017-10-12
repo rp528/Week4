770a0d279b70af6c0973df79a04392684a7a0692
ravi patel <rp528@osl25.njit.edu>
2017-10-11 21:30:44 -0400

    done

--- index.php
+++ index.php
@@ -1,38 +1,52 @@
 <?php
-	$date =  date('Y/m/d', time());
+	$date =  date('Y-m-d', time());
 	echo "The value of \$date: ".$date."<br>";
 	$tar = "2017/05/24";
 	echo "The value of \$tar: ".$tar."<br>";
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
+	$date =  date('Y/m/d', time());
 
-	echo "2). $date";
-	echo "3)";
+
+	echo "<br>";
+	echo "2) $date <br>";
+	echo "3) ";
 	
 	if ($date > $tar)
-		echo "The Future";
+		echo "The Future <br>";
 	else if ($date < $tar)
-		echo "The Past";
-	else ($date == $tar)
-		echo "Oops";
+		echo "The Past <br>";
+	else if ($date == $tar)
+		echo "Oops <br>";
 
-	echo "4)". strpos($date, "/") . stripos($date, "/"); 
+	echo "4) ". strpos($date, "/") ." ". strripos($date, "/")."<br>"; 
 
-	echo "5)" . str_word_count($date);
+	echo "5) " . str_word_count($date). "<br>";
 	
-	echo "6)" . strlen($date);
+	echo "6) " . strlen($date). "<br>";
 	
-	echo "7)" . ord($date);
+	echo "7) " . ord($date). "<br>";
 	
-	echo "8)" . substr($date, -2);
+	echo "8) " . substr($date, -2). "<br>";
 
-	echo "9)" . print_r(explode('/', $date);
+	echo "9) ";
 	
+	print_r(explode('/', $date));
 	
+	echo "<br>";
+
+	echo "10) "; 
 	
-	
-	
+	for ($i = 0; $i < count($year); $i++) {
+
+	if ((($year[$i] % 4) == 0) && ((($year[$i] % 100) != 0) || (($year[$i] % 400) == 0)))
+			echo $year[$i].": True | ";
+		else 
+			echo $year[$i] . ": False | ";
+
+		
+	}
 	
 
 ?>
